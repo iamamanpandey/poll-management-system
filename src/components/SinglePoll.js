594e7c2d759b11578f3df693ab8a217a6ca767ec
iamamanpandey <iamamanpandey007@gmail.com>
2021-06-17 16:54:33 +0530

    add delete option

--- src/components/SinglePoll.js
+++ src/components/SinglePoll.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { reqPollById, deletePollReq } from "../redux/actions";
+import { reqPollById, deletePollReq, deleteOptionReq } from "../redux/actions";
 import { Link } from "react-router-dom";
 import Option from "./AddOption";
 
@@ -33,19 +33,29 @@ const SinglePost = (props) => {
               <div class="d-flex flex-row justify-content-between align-items-center question-title  border-bottom">
                 <h5 class="mt-1 ml-2">{poll.data.title}</h5>
                 <Link to={`/admin/polls/edittitle/${poll.data._id}`}>
-                <button
-                  class="btn btn-primary border-success align-items-center btn-success"
-                  type="button"
-                  onClick={() => dispatch(reqPollById(poll.data._id))}
-                >
-                  update Title <i class="fa fa-angle-right ml-2"></i>
-                </button>
-              </Link>
+                  <button
+                    class="btn btn-primary border-success align-items-center btn-success"
+                    type="button"
+                    onClick={() => dispatch(reqPollById(poll.data._id))}
+                  >
+                    update Title <i class="fa fa-angle-right ml-2"></i>
+                  </button>
+                </Link>
               </div>
             </div>
             {poll.data.options.map((option) => (
               <div className=" text-center">
-              <label class="btn btn-outline-success w-50 ">{option.option}</label></div>
+                <label class="btn btn-outline-success w-50 ">
+                  {option.option}
+                  <button
+                    class="btn btn-primary  float-right btn-danger"
+                    type="button"
+                    onClick={() => dispatch(deleteOptionReq({id:poll.data._id,text:option.option}))}
+                   >
+                    <i class="fa fa-angle-left mt-1 mr-1"></i>&nbsp;Delete
+                  </button>
+                </label>
+              </div>
             ))}
             <div class="d-flex flex-row justify-content-between align-items-center p-3 bg-white">
               <button
