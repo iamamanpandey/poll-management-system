b6974be9e47b0a47e6532e89b0f099f972f06e3a
iamamanpandey <iamamanpandey007@gmail.com>
2021-06-26 17:38:38 +0530

    removed nav bar

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import "react-toastify/dist/ReactToastify.css";
 import Routes from "./Routes";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 function App() {
-  const token = localStorage.getItem("token", token);
 
   return (
     <div className="App ">
