bc3aefcb510e51d93b80a7b9d678ede0caf82b31
iamamanpandey <iamamanpandey007@gmail.com>
2021-07-05 19:04:45 +0530

    add react

--- src/App.js
+++ src/App.js
@@ -1,34 +1,75 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ApiCalendar from 'react-google-calendar-api';
 
 const App = () => {
+  const [event, setEvent] = useState([])
+  const [login, setlogin] = useState(false)
 
   const handleItemClick = (e, name) => {
     if (name === 'sign-in') {
       ApiCalendar.handleAuthClick();
+      console.log("status true", ApiCalendar)
+      setlogin(true)
     } else if (name === 'sign-out') {
       ApiCalendar.handleSignoutClick();
+      setlogin(false)
+      console.log("status", ApiCalendar.sign)
     }
   }
+
+
+  useEffect(() => {
+    if (ApiCalendar.sign)
+      ApiCalendar.listUpcomingEvents().then(({ result }) => {
+        setEvent(result.items)
+
+        console.log(event)
+      });
+  }, [])
+
   return (
     <div>
+      {login === false ?
 
-      <button
-        onClick={(e) => handleItemClick(e, 'sign-in')}
-      >
-        sign-in
+        <button
+          onClick={(e) => handleItemClick(e, 'sign-in')}
+        >
+          sign-in
               </button>
-      <button
-        onClick={(e) =>handleItemClick(e, 'sign-out')}
-      >
-        sign-out
-              </button>
+        :
 
-
+        <div>
+          {event &&
+            event.map((item) => {
+              return (
+                <div className="w-50  mx-auto">
+                  <div className="card my-2" >
+                    <div className="card-header d-flex justify-content-between">
+                      <p>{item.created}</p>
+                      <p>{item.end.dateTime}</p>
+                    </div>
+                    <div className="card-body">
+                      <h5 className="card-title">{item.summary}</h5>
+                      <p className="card-text">
+                        {item.description}
+                      </p>
+                      <p className="card-text">
+                        {item.location}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+          <button
+            onClick={(e) => handleItemClick(e, 'sign-out')}
+          >
+            sign-out
+          </button>
+        </div>
+      }
     </div>
   )
-
-
 }
 
 export default App;
--- src/Card.js
+++ src/Card.js
@@ -1,1 +1,58 @@
-import React  from 'react';
+import React from "react";
+
+export default function Card() {
+
+var x = Math. floor(Math. random() * 256);
+var y = Math. floor(Math. random() * 256);
+var z = Math. floor(Math. random() * 256);
+var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+var bgColor2 = "rgb(" + z + "," + x + "," + y + ")";
+var bgColor3 = "rgb(" + y + "," + z + "," + x + ")";
+
+
+
+  return (
+    <div className="w-50  mx-auto">
+      <div className="card my-2" style={{backgroundColor:`${bgColor}`}} >
+        <div className="card-header">Featured</div>
+        <div className="card-body">
+          <h5 className="card-title">Special title treatment</h5>
+          <p className="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+        </div>
+      </div>
+      <div className="card my-2" style={{backgroundColor:`${bgColor2}`}}>
+        <div className="card-header">Featured</div>
+        <div className="card-body">
+          <h5 className="card-title">Special title treatment</h5>
+          <p className="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+        </div>
+      </div>
+      <div className="card my-2" style={{backgroundColor:`${bgColor3}`}}>
+        <div className="card-header">Featured</div>
+        <div className="card-body ">
+          <h5 className="card-title">Special title treatment</h5>
+          <p className="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+        </div>
+      </div>
+      <div className="card my-2" style={{backgroundColor:`${bgColor}`}}>
+        <div className="card-header">Featured</div>
+        <div className="card-body">
+          <h5 className="card-title">Special title treatment</h5>
+          <p className="card-text">
+            With supporting text below as a natural lead-in to additional
+            content.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
